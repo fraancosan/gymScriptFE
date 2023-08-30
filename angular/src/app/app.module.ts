1ae79d4dd914aa034c638a3c2a6118a53d9efdf9
Franco Sanchez <78979358+francosanchez8102@users.noreply.github.com>
2023-08-30 18:10:23 +0000

    se arreglan conflictos merge

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import { LoadingComponent } from './components/loading/loading.component';
->>>>>>> refs/remotes/origin/main
 
 @NgModule({
   declarations: [
