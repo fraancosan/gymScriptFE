1c4665ca2c6c05a6ebbb03f0f3228a1dbfd1fced
NicoFani <nicolasfani87@gmail.com>
2023-09-03 15:12:01 -0300

    Se hace la modificacion del header al scrollear, y se corrige la poiscion de los elementos del mismo

--- angular/src/app/components/header/header.component.ts
+++ angular/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  scrolled = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('window:scroll',['$event'])
+
+  onScroll(event: Event): void {
+    if (window.scrollY > 0) {
+      this.scrolled = true;
+      this.renderer.addClass(document.querySelector('.header'), 'in-scroll');
+    } else {
+      this.scrolled = false;
+      this.renderer.removeClass(document.querySelector('header'),'in-scroll');
+    }
+  }
 }
