66566c7a3f11cd7356e738c82663a04a4d06830d
utnros <142024812+utnros@users.noreply.github.com>
2023-09-06 15:37:22 +0000

    Implementacion rutas

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path: 'registro', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ angular/src/app/register/register.component.html
@@ -0,0 +1,1 @@
+<h1>REGISTRO</h1>
