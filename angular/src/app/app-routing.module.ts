83ede6b962c66d9cb9e75e199bdb73d1767c57f8
Franco Sanchez <78979358+francosanchez8102@users.noreply.github.com>
2023-09-15 14:51:58 +0000

    se configura para pdoer hacer deploy

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProductosComponent } from './productos/productos.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path: 'signUp', component: RegisterComponent},
-  {path: 'signIn', component: LoginComponent}
+  {path: 'signIn', component: LoginComponent},
+  {path: 'productos', component: ProductosComponent}
 ];
 
 @NgModule({
