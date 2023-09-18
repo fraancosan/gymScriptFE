ac341cc9299a403149b1985f967d6acafcfdc251
Franco Sanchez <francosanchez8102@gmail.com>
2023-09-18 18:57:42 -0300

    se modifica loading

--- angular/src/app/components/loading/loading.component.ts
+++ angular/src/app/components/loading/loading.component.ts
@@ -5,6 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css']
 })
-export class LoadingComponent {
 
+export class LoadingComponent {
+  ngOnInit(): void {
+    let loading = document.getElementById('loading');
+    if (window.innerWidth >= 500) {
+      document.body.style.overflow = 'hidden';
+      
+      if (loading) {
+        loading.classList.remove('hidden');
+      }
+
+      setTimeout(() => {
+        document.body.style.overflow = 'auto';
+        if (loading) {
+          loading.classList.add('hidden');
+        }
+      },2600);
+    } 
+  }
 }
