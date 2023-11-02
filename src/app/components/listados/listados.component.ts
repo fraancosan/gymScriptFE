d28f476d50ad515887b422e6f0447b5ad9a6dc51
Franco Sanchez <francosanchez8102@gmail.com>
2023-11-01 23:36:10 -0300

    fix url

--- src/app/components/listados/listados.component.ts
+++ src/app/components/listados/listados.component.ts
@@ -20,7 +20,7 @@ export class ListadosComponent{
    ngOnInit(): void {
     let tipos = this.identifyService.identificar(this.tabla);
     // se obtienen los datos de la base de datos
-    this.http.get("https://servidordsw.onrendereee.com/" + this.tabla).pipe(
+    this.http.get("https://servidordsw.onrender.com/" + this.tabla).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status == 404){
           // Si hay error 404 devuelvo array vacio para que no de error al cargar el componente, AUTOMATIZAR
