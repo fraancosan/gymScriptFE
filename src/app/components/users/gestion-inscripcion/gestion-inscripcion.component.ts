73014a232c65c43d45217eab47f8f38966fa3ac2
Franco Sanchez <francosanchez8102@gmail.com>
2023-11-18 19:44:02 -0300

    se añade gestion de inscripcion

--- src/app/components/users/gestion-inscripcion/gestion-inscripcion.component.ts
+++ src/app/components/users/gestion-inscripcion/gestion-inscripcion.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConeccionService } from 'src/app/services/bd/coneccion.service';
 
 @Component({
   selector: 'app-gestion-inscripcion',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./gestion-inscripcion.component.css']
 })
 export class GestionInscripcionComponent {
+  @Input() inscripcion: any = {};
+  descripcionPlan: string = '';
+  localidad: any = {};
+  fecha: any;
 
+  constructor(
+    private bd: ConeccionService, 
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    console.log(this.inscripcion);
+    this.descripcionPlan = this.inscripcion.plan.descripcion.split('-')
+
+    this.bd.getOne('localidades', "Localidad", this.inscripcion.sede.idLocalidad).subscribe((data: any) => {this.localidad = data});
+
+    let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    this.fecha = new Date(this.inscripcion.fechaAlta + 'T00:00:00');
+    this.fecha = this.fecha.getDate() + ' de ' + meses[this.fecha.getMonth()] + ' de ' + this.fecha.getFullYear();
+  }
+
+  darDeBaja() {
+    // Primero se pone un mensaje de confirmación
+    if (confirm('¿Está seguro que desea dar de baja esta inscripción?')) {
+      // Json a enviar
+      let baja = {
+        "id": this.inscripcion.id,
+        "fechaBaja": new Date(),
+      }
+      this.bd.update('inscripciones', baja).subscribe((data: any) => {this.router.navigate(["/home"])});
+    }
+  }
+
+  cambiarPlan() {
+    // Primero se pone un mensaje de confirmación
+    if (confirm('¿Está seguro que desea cambiar el plan o la sede? Se le volvera a cobrar la inscripción')) {
+      // Json a enviar
+      let baja = {
+        "id": this.inscripcion.id,
+        "fechaBaja": new Date(),
+      }
+      this.bd.update('inscripciones', baja).subscribe((data: any) => {window.location.reload()});
+    }
+  }
 }
