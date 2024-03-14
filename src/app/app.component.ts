f7c1134fb46d8ca65f8c9bc439277cced6091917
ManuMong <69636953+ManuMong@users.noreply.github.com>
2024-03-14 21:53:30 +0000

    fix loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { HeaderServiceService } from './services/header-service.service';
 
 @Component({
@@ -8,8 +9,25 @@ import { HeaderServiceService } from './services/header-service.service';
 })
 export class AppComponent {
   primeraVez: boolean = true;
+  isLoading = true;
+  isHomeRoute = false;
 
-  constructor(public headerService: HeaderServiceService) {}
+  constructor(public headerService: HeaderServiceService, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isHomeRoute = event.url === '/home' || event.url === '/';
+        if (this.primeraVez && this.isHomeRoute) {
+          this.isLoading = true;
+          this.primeraVez = false;
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 2600);
+        } else {
+          this.isLoading = false;
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {}
 }
